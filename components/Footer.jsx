56b93f06d321b0fa51e294b67f50cc3d336c8b56
Rahul <himanshu.lodhi@softevia.com>
2025-11-26 00:21:36 +0530

    fixes 2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -52,7 +52,7 @@ const Footer = () => {
                 <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-slate-500/30 text-slate-500">
                     <div>
                         <Link href="/" className="text-4xl font-semibold text-slate-700">
-                            <span className="text-green-600">Sphere</span>Trends<span className="text-green-600 text-5xl leading-0">.com</span>
+                            <span className="text-green-600">Sphere</span>Trends
                         </Link>
                         <p className="max-w-[410px] mt-6 text-sm">Welcome to SphereTrends, your ultimate destination for the latest and smartest gadgets. From smartphones and smartwatches to essential accessories, we bring you the best in innovation — all in one place.</p>
                         <div className="flex items-center gap-3 mt-5">
